9bbbe69e7e65e47da22d9a2fbd2631782e59f62d
Ahmad <ahmedmageed19@gmail.com>
2022-03-13 19:33:50 +0000

    Sync LeetCode submission - Valid Parentheses (cpp)

--- LeetCodeSolutions/problems/valid_parentheses/solution.cpp
+++ LeetCodeSolutions/problems/valid_parentheses/solution.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-bool isValid(string s){
-    // Loop each element in string 
-    // if oppenning Parentheses add one to VALIDATION
-    // if clossing Parentheses minus one to VALIDATION
-    // return true only if VALID is equal to zero
-    stack<char> ORDER;
-    for(auto p: s){
-        switch (p)
-        {
-            case '{': ORDER.push('}'); break;
-            case '(': ORDER.push(')'); break;
-            case '[': ORDER.push(']'); break;
-            default:
-                if(ORDER.empty() || p != ORDER.top()) return false;
-                else ORDER.pop();
+    bool isValid(string s) {
+        stack<char> qParenthes;
+        map<char, char> brackets={
+            {'(', ')'},
+            {'{', '}'},
+            {'[', ']'}
+        };
+        
+        for(auto p: s){
+            // current char is an opening parentheses 
+            if(brackets.find(p) != brackets.end()){
+                qParenthes.push(p);
+            } else {
+                if(!qParenthes.empty() && brackets[qParenthes.top()] == p){
+                  qParenthes.pop();  
+                } else {
+                    return false;
+                }
+            }
         }
+        
+        return qParenthes.empty();
     }
-    return ORDER.empty();
-}
-
 };
