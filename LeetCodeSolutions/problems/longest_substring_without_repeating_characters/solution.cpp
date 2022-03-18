cac617c05a52bbe2f1154b6d6bd53025182e47df
Ahmad <ahmedmageed19@gmail.com>
2022-03-18 16:44:18 +0000

    Sync LeetCode submission - Longest Substring Without Repeating Characters (cpp)

--- LeetCodeSolutions/problems/longest_substring_without_repeating_characters/solution.cpp
+++ LeetCodeSolutions/problems/longest_substring_without_repeating_characters/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        int res = 0;
-        int i=0;
-        map<char, int> hash;
-        
-        for(int j=0;j<s.size();j++){
-            if(hash.find(s[j]) != hash.end()){
-                i = max(hash[s[j]], i);
-            }
-            res = max(res, j-i+1);
-            hash[s[j]] = j+1;
+int lengthOfLongestSubstring(string s) {
+    int res = 0;
+    int i=0;
+    set<char> found;
+    
+    for(int j=0 ; j < s.size(); j++){
+        //  remove Repeating chars from substring and count current lenght
+        while(found.find(s[j]) != found.end()){
+            found.erase(s[i]);
+            i++;
         }
-        return res;
-
+        found.insert(s[j]);
+        res = max(res, j-i+1);
     }
+    return res;
+}
+
 };
