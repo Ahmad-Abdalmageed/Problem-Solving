e117e5c8932048dc94d09b98d6e020eaad5c34bf
Ahmad <ahmedmageed19@gmail.com>
2022-05-05 13:07:52 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node II (cpp)

---
+++ LeetCodeSolutions/problems/populating_next_right_pointers_in_each_node_ii/solution.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node *connect(Node *root) {
+      if(root==nullptr) return root;
+      Node* head = root;
+
+      while(head != nullptr){
+        Node* dummy = new Node(0);
+        Node* temp = dummy;
+        // Level Traversal
+        while(head != nullptr){
+          if(head->left != nullptr){
+            temp->next = head->left;
+            temp = temp->next;
+          }
+          if(head->right != nullptr){
+            temp->next = head->right;
+            temp = temp->next;
+          }
+          head = head->next;
+        }
+        head = dummy->next;
+      }
+      return root;
+    }
+
+};
