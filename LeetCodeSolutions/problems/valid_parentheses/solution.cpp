840dabbdaf1fe2f338ee85279d6b898b5ee77a88
Ahmad <ahmedmageed19@gmail.com>
2022-07-15 13:29:29 +0000

    Sync LeetCode submission - Valid Parentheses (cpp)

--- LeetCodeSolutions/problems/valid_parentheses/solution.cpp
+++ LeetCodeSolutions/problems/valid_parentheses/solution.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<char> qParenthes;
-        map<char, char> brackets={
-            {'(', ')'},
-            {'{', '}'},
-            {'[', ']'}
-        };
-        
-        for(auto p: s){
-            // current char is an opening parentheses 
-            if(brackets.find(p) != brackets.end()){
-                qParenthes.push(p);
-            } else {
-                if(!qParenthes.empty() && brackets[qParenthes.top()] == p){
-                  qParenthes.pop();  
-                } else {
-                    return false;
-                }
-            }
-        }
-        
-        return qParenthes.empty();
+  bool isValid(string s)
+  {
+    // for each opening Parentheses add its closing one
+    stack<char> parenthesesSequence;
+    for (auto const &bracket : s)
+    {
+      switch (bracket)
+      {
+      case '{':
+        parenthesesSequence.push('}');
+        break;
+      case '(':
+        parenthesesSequence.push(')');
+        break;
+      case '[':
+        parenthesesSequence.push(']');
+        break;
+      default:
+        // When adding a closing bracket without it's openning one 
+        // retrun false
+        if (parenthesesSequence.empty() || bracket != parenthesesSequence.top())
+          return false;
+        // when adding closing bracket that match the closing ones added in the
+        // stack -> pop the stack
+        else
+          parenthesesSequence.pop();
+      }
     }
+    // return true only if stack is empty after operations
+    return parenthesesSequence.empty();
+  }
+
 };
