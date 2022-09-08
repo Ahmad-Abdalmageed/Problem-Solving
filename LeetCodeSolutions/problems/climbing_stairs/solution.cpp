ef5ea497cdf175b2a51c4f7f4033450eb2b20acd
Ahmad <ahmedmageed19@gmail.com>
2022-09-08 17:13:34 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

--- LeetCodeSolutions/problems/climbing_stairs/solution.cpp
+++ LeetCodeSolutions/problems/climbing_stairs/solution.cpp
@@ -1,13 +1,44 @@
 class Solution {
 public:
+    // Recursion + Memoization
     int climbStairs(int n) {
-        int one = 1, two = 1;
+        vector<int> memo(n + 1, 0);
+        return dfs(0, n, memo);
+    }
+    
+private: 
+    
+    int dfs(int curNode, int n, vector<int> &memo){
+        if(curNode > n)
+            return 0;
+    
+        if(memo[curNode] != 0)
+            return memo[curNode];
         
-        for(int i = 0; i < n-1; i++){
-            int temp = one;
-            one = one+two;
-            two = temp;
-        }
-        return one;
+        if(curNode == n)
+            return 1;
+        
+        memo[curNode] = dfs(curNode + 1, n, memo) + dfs(curNode + 2, n, memo);
+        return memo[curNode];
+        
     }
 };
+
+    // Tabulation Solution
+//     int climbStairs(int n) {
+//         // Base Case 
+//         if(n == 1) return 1;
+        
+//         vector<int> stairs(n+1, 0);
+//         stairs[1] = 1;
+//         stairs[2] = 1;
+        
+//         for(int iStair = 0; iStair < n+1 ; iStair++){
+//             if(iStair + 1 < n + 1)
+//                 stairs[iStair + 1] += stairs[iStair] ;
+//             if(iStair + 2 < n + 1)
+//                 stairs[iStair + 2] += stairs[iStair];
+//         }
+        
+//         return stairs[n];
+//     }
