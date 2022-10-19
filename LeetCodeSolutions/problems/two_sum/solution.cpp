4f1b413711e678a4c71ec1a18e2257a509d592ba
Ahmad <ahmedmageed19@gmail.com>
2022-10-19 16:45:32 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- LeetCodeSolutions/problems/two_sum/solution.cpp
+++ LeetCodeSolutions/problems/two_sum/solution.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    // Using Maps
-    std::vector<int> twoSum(vector<int> &nums, int target) {
-      std::vector<int> res;
-      //  Store Each element and its index
-      std::unordered_map<int, int> table;
-
-      for (unsigned int i = 0; i < nums.size(); i++) {
-        //  Compute the Complementary
-        int comp = target - nums[i];
-
-        // If the Complementary is an element existing in the Array
-        // Push the Index of the Current element and index of the Complementary 
-        // Element
-        if (table.find(comp) != table.end()) {
-          res.push_back(i);
-          res.push_back(table[comp]);
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // store the indices 
+        unordered_map<int, int> remainderMap;
+        vector<int> solution;
+        
+        
+        for(int indx = 0; indx < nums.size(); indx++)
+        {
+            int remainder = target - nums[indx];
+            
+            if(remainderMap.find(remainder) != remainderMap.end())
+            {
+                solution.push_back(indx);
+                solution.push_back(remainderMap[remainder]);
+            }
+            remainderMap[nums[indx]] = indx;
         }
-
-        // if not in table insert current element 
-        table.insert({nums[i], i});
-      }
-
-      return res;
+        
+        return solution;
     }
-};  
-
-
-
+};
